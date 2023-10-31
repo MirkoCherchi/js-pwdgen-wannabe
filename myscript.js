9adf730d27c3711dc14ea18bc207ecbfe9b66254
MirkoCherchi <mirkocherchi1992@gmail.com>
2023-10-31 15:48:41 +0100

    feat: with favorite number

--- myscript.js
+++ myscript.js
@@ -1,2 +1,32 @@
 'use strict';
 
+const nameUser = document.getElementById('nome');
+const surnameUser = document.getElementById('cognome');
+const colorUser = document.getElementById('colore');
+const numberUser = document.getElementById('numero');
+
+
+
+nameUser.innerHTML = 'Nome: ';
+surnameUser.innerHTML = 'Cognome: ';
+colorUser.innerHTML = 'Colore preferito: ';
+numberUser.innerHTML = 'Numero preferito: ';
+
+const nameInput = prompt('Inserisci il tuo Nome');
+const surnameInput = prompt('Inserisci il tuo Cognome');
+const colorInput = prompt('Inserisci il tuo colore preferito');
+const numberInput = prompt('Inserisci il tuo numero preferito');
+
+
+nameUser.innerHTML += nameInput
+surnameUser.innerHTML += surnameInput
+colorUser.innerHTML += colorInput
+numberUser.innerHTML += numberInput;
+
+console.log(nameInput);
+console.log(surnameInput);
+console.log(colorInput);
+console.log(parseInt(numberInput));
+
+
+
